3926b66482db27be8450d773589ffc6b9393c049
Praveen Kumar Jha <praveen@gamp.gg>
2025-06-06 14:11:08 +0530

    Remove deals-api directory and all associated files, including configuration, source code, and documentation, to streamline project structure.

--- deals-api/src/deals/dto/update-deal-detail.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDealDetailDto } from './create-deal-detail.dto';
-
-export class UpdateDealDetailDto extends PartialType(CreateDealDetailDto) {}
